a1f0c79af722e9fc3d75a75932baaddf14ca9683
Amarse <byicy08@gmail.com>
2020-05-17 14:07:20 +0900

    자바스크립트 / work  필터링 추가

--- js/main.js
+++ js/main.js
@@ -60,3 +60,36 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
+//프로젝트 메뉴 누르면 해당 프로젝트 열리기
+const workMenuBtn = document.querySelector('.work__categories');
+const projectMenuBtn = document.querySelector('.works__projects');
+const projects = document.querySelectorAll('.project');
+
+workMenuBtn.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+  // 3초 있다 필터링 노출하기
+  projectMenuBtn.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      // console.log(project.dataset.type);
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+
+    projectMenuBtn.classList.remove('anim-out');
+  }, 300);
+
+  // let project = 0;
+  // for (let i = 0; i < projects.length; i++) {
+  //   project = projects[i];
+  //   console.log(project);
+  // }
+});
